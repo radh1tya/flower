5c017282182ef11eb10b3212b44520005109f4fc
alif <alif@localhost.localdomain>
2024-09-27 22:40:00 +0700

    hmm

--- parsing.c
+++ parsing.c
@@ -6,8 +6,12 @@
 MarkdownElement detect_md(char *line) {
   MarkdownElement element;
   if(line[0] == '#') {
-    element.type = HEADING;
-    element.content = strdup(line + 2);
+    int count = 0;
+    while (line[count] == '#') {
+      count++;
+    }
+    element.type = count;
+    element.content = strdup(line + count + 2);
   } else {
     element.type = PARAGRAPH;
     element.content = strdup(line);
@@ -19,8 +23,8 @@ void parsing(FILE *file, FILE *fw) {
   while (fgets(line, sizeof(line), file)) {
     MarkdownElement element = detect_md(line);
 
-    if ( element.type == HEADING ) {
-      fprintf(fw, "<h1>%s</h1>\n", element.content);
+    if ( element.type >= 1 && element.type <= 6 ) {
+      fprintf(fw, "<h%d>%s</h%d>\n", element.type, element.content, element.type);
     } else if (element.type == PARAGRAPH) {
       fprintf(fw, "<p>%s</p>\n", element.content);
     }
