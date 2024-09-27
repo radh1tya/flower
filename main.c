6f6451dd5bfdae2ad9f40f25402845d584118b9e
alif <alif@localhost.localdomain>
2024-09-27 19:49:08 +0700

    mantap

--- main.c
+++ main.c
@@ -1,23 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+void parsing(char *filecontents, FILE *fw) {
+
+  if(filecontents[0] == '#') {
+    int count = 0;
+
+    while(filecontents[count] == '#') {
+      count++;
+    }
+    
+    fprintf(fw, "<h%d>", count);
+    fprintf(fw, "%s", filecontents + count + 1);
+    fprintf(fw, "</h%d>\n", count);
+  }
+
+  else {
+    fprintf(fw, "<p>%s</p>\n", filecontents);
+  }
+  
+}
 
 int
-main() {
+main(int argc, char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "Penggunaan: %s <filename>\n", argv[0]);
+    return 1;
+  }
   
-  FILE *fp;
-  char isipesan[100];
-  
-  fp = fopen("post/pop.md", "r");
+  char *filename = argv[1];
+  char filecontents[100];
+  FILE *fp, *fw;
+
+  fp = fopen(filename, "r");
 
   if (fp == NULL) {
     fprintf(stderr, "404\n");
     exit(1);
   }
 
-  while(fgets(isipesan, 100, fp) != NULL) {
-  printf("%s", isipesan);
+  fw = fopen("output.html", "w");
+  if (fw == NULL) {
+    fprintf(stderr, "Galat membuat keluaran berkas\n");
+    return 1;
   }
+
+  while(fgets(filecontents, sizeof(filecontents), fp) != NULL) {
+    parsing(filecontents, fw);
+  }
+  
   fclose(fp);
+  fclose(fw);
   
   return 0;
 }
