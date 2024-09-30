f7241a80e0cebb0ddff878d2151815f1c6adcf45
Yuuki1578 <destuawang@gmail.com>
2024-09-30 13:38:54 +0700

    pesan commit di commit sebelumnya :v, lihat di 'git log'

--- src/main.c
+++ src/main.c
@@ -2,18 +2,18 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define root "post"
 
-int is_markdown_file(const char *filename) {
-  const char *ext = strrchr(filename, '.');
-  return (ext && strcmp(ext, ".md") == 0);
-}
+int is_markdown_file(const char *);
 
 int main(void) {
   struct dirent *de;
-  DIR *dr = opendir("post");
+  DIR *dr;
+
+  dr = opendir(root);
 
   if (dr == NULL) {
-    fprintf(stderr, "404 'post'\n");
+    fprintf(stderr, "404 directory '%s' not found\n", root);
     return 1;
   }
 
@@ -21,36 +21,55 @@ int main(void) {
     if (de->d_type == DT_DIR)
       continue;
 
-    if (is_markdown_file(de->d_name)) {
-      char input_file[256];
-      snprintf(input_file, sizeof(input_file), "post/%s", de->d_name);
+    if (is_markdown_file(de->d_name) && de->d_type == DT_REG) {
 
-      FILE *fp = fopen(input_file, "r");
+      // @WARNING Segmentation fault
+      // untuk variable input file, lebih baik memorinya
+      // disimpan di heap dengan malloc()
+      // @TODO alokasi 256 bytes heap memori ke input_file
+      char input_file[256];
+      FILE *fp;
+
+      snprintf(input_file, sizeof(input_file), "post/%s", de->d_name);
+      fp = fopen(input_file, "r");
+
       if (fp == NULL) {
-        fprintf(stderr, "404 %s\n", input_file);
-        exit(1);
+        fprintf(stderr, "404 expected markdown files in %s\n", input_file);
+        return 1;
       }
 
       char output_file[256];
-      snprintf(output_file, sizeof(output_file), "public/%s", de->d_name);
+      char *ext;
 
-      char *ext = strrchr(output_file, '.');
+      snprintf(output_file, sizeof(output_file), "public/%s", de->d_name);
+      ext = strrchr(output_file, '.');
+
       if (ext != NULL) {
         strcpy(ext, ".html");
       }
+
       include_style();
+
       FILE *fw = fopen(output_file, "w");
+
       if (fw == NULL) {
         fprintf(stderr, "Galat membuat keluaran berkas %s\n", output_file);
         fclose(fp);
+
         continue;
       }
 
       parsing(fp, fw);
-
       fclose(fp);
       fclose(fw);
     }
   }
+
   return 0;
+}
+
+int is_markdown_file(const char *filename) {
+  const char *ext = strrchr(filename, '.');
+
+  return !strcmp(ext, ".md");
 }
